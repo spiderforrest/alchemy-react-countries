834087c35f817d08516e3f6c04aecfe12d3c7ffa
spiderforrest <spider@spood.org>
2023-01-18 12:36:22 -0800

    filter working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tile from './Tile.js';
 import { useCountries, useContinents } from '../hooks/countries.js';
 
 export default function Main() {
   const countries = useCountries();
   const continents = useContinents();
+  const [filter, setFilter] = useState('');
 
   return (
     <div className="Main">
-      <select>
+      <select onChange={(e) => setFilter(e.target.value)}>
         {continents.map((continent) => (
           <option key={continent} value={continent}>
             {continent}
           </option>
         ))}
       </select>
-      {countries.map((country) => (
-        <Tile key={country.id} {...country} />
-      ))}
+      {countries
+        .filter((country) => country.continent === filter)
+        .map((country) => (
+          <Tile key={country.id} {...country} />
+        ))}
     </div>
   );
 }
